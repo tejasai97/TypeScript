b25dba7ca7c1d0aff30e9abda28e00819e6efe8b
tejasai97 <teja.g14@gmail.com>
2017-06-05 19:44:10 +0530

    first

---
+++ main.js
@@ -0,0 +1,9 @@
+var SweetSweetClass = (function () {
+    function SweetSweetClass() {
+    }
+    SweetSweetClass.prototype.constuctor = function () {
+        console.log("even sweeter");
+    };
+    return SweetSweetClass;
+}());
+var basil = new SweetSweetClass();
---
+++ main.ts
@@ -0,0 +1,6 @@
+class SweetSweetClass{
+    constuctor(){
+        console.log("even sweeter")
+    }
+}
+let basil =new SweetSweetClass()
