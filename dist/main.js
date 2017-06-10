a46604bada953fe51e3f643b14eff6ae7d8cfb73
Gadipally Sai Teja <teja.g14@gmail.com>
2017-06-10 12:12:46 +0530

    classes and inheritence

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,37 @@
-var SweetSweetClass = (function () {
-    function SweetSweetClass() {
-        console.log("Even  more sweeter");
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var SweetSweetBasil = (function () {
+    function SweetSweetBasil(name, startColor) {
+        console.log("hello " + name);
+        this.color = startColor;
     }
-    return SweetSweetClass;
+    SweetSweetBasil.prototype.getColor = function () {
+        console.log(this.color);
+    };
+    return SweetSweetBasil;
 }());
-var basil = new SweetSweetClass();
+var basil = new SweetSweetBasil('teja', 'bright green');
+basil.getColor();
+console.log(basil.color);
+var Basil = (function (_super) {
+    __extends(Basil, _super);
+    function Basil(name, startColor) {
+        return _super.call(this, name, startColor) || this;
+    }
+    Basil.prototype.setNewColor = function (newcolor) {
+        this.color = newcolor;
+    };
+    return Basil;
+}(SweetSweetBasil));
+var basil2 = new Basil("basil", "blue");
+basil2.getColor();
+basil2.setNewColor("red");
+basil2.getColor();
--- main.ts
+++ main.ts
@@ -1,6 +1,31 @@
-class SweetSweetClass {
-    constructor() {
-        console.log("Even  more sweeter")
-    }
+class SweetSweetBasil {
+  color:string;
+  constructor(name:string,startColor:string){
+    console.log("hello "+name)
+    this.color = startColor
+  }
+  getColor(){
+    console.log(this.color)
+  }
 }
-let basil = new SweetSweetClass()
+let basil = new SweetSweetBasil('teja','bright green')
+
+basil.getColor()
+
+console.log(basil.color)
+
+class Basil extends SweetSweetBasil{
+  constructor(name:string,startColor:string){
+    super(name,startColor)
+  }
+  setNewColor(newcolor:string){
+    this.color=newcolor
+  }
+}
+
+
+let basil2= new Basil("basil","blue")
+
+basil2.getColor()
+basil2.setNewColor("red")
+basil2.getColor()
